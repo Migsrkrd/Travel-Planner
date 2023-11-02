319c7764869af36c3658b7b085fd5c7e62571ba1
Ezekiel <ezekieljamolin186@gmail.com>
2023-11-02 11:24:10 -0700

    added traveler table

--- models/Traveler.js
+++ models/Traveler.js
@@ -5,7 +5,30 @@ const sequelize = require('../config/connection.js');
 class Traveler extends Model {}
 
 Traveler.init(
-  
+   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isAlphanumeric: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
 );
 
 module.exports = Traveler;
