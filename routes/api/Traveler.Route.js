f7276b3d051c997847d88aad0559d0039fd175f5
Ezekiel <ezekieljamolin186@gmail.com>
2023-11-02 11:43:21 -0700

    made routes for traveler

--- routes/api/Traveler.Route.js
+++ routes/api/Traveler.Route.js
@@ -5,11 +5,11 @@ const { Traveler, Trip } = require('../../models');
 
 router.get('/', async (req, res) => {
   try{
-    const categories = await Category.findAll({
+    const travelerData = await Traveler.findAll({
       include: [{
-        model: Product}]
+        model: Trip}]
     });
-    res.json(categories);
+    res.json(travelerData);
   } catch (err) {
     res.status(500).json({err: 'Internal Server Error'})
     console.error(err);
@@ -20,14 +20,14 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try{
-    const categories = await Category.findByPk(req.params.id, {
-      include: [{model: Product}],
+    const travelerData = await Traveler.findByPk(req.params.id, {
+      include: [{model: Trip}],
     });
-    if(!categories) {
-      res.status(404).json({message: 'No Categories under that id'});
+    if(!travelerData) {
+      res.status(404).json({message: 'No traveler under that id'});
       return;
     }
-    res.status(200).json(categories);
+    res.status(200).json(travelerData);
   } catch(err) {
     res.status(500).json({message: "Server error"})
   }
@@ -37,8 +37,8 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try{
-    const categories = await Category.create(req.body);
-    res.status(200).json(categories);
+    const travelerData = await Traveler.create(req.body);
+    res.status(200).json(travelerData);
   } catch(err) {
     res.status(400).json(err)
   }
@@ -47,13 +47,13 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   try{
-    const categories = await Category.update(req.body, {
+    const travelerData = await Traveler.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
-    if(!categories[0]){
-      res.status(400).json({message: 'No category with that id'})
+    if(!travelerData[0]){
+      res.status(400).json({message: 'No traveler with that id'})
     }
     res.status(200).json({message: "Value successfully changed!"});
   } catch (err) {
@@ -64,12 +64,12 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try{
-    const categories = await Category.findByPk(req.params.id);
-    if(!categories){
-      res.status(400).json({message: 'No category with that id'});
+    const travelerData = await Traveler.findByPk(req.params.id);
+    if(!travelerData){
+      res.status(400).json({message: 'No traveler with that id'});
       return
     }
-    await categories.destroy();
+    await travelerData.destroy();
     res.status(200).json({message: "Value successfully deleted!"});
   } catch (err) {
     res.status(500).json(err);
