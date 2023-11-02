6d956505b7e7bc5bfbfa702692cdc0540b518cb7
Mike <mreickcastillero@gmail.com>
2023-11-02 11:31:24 -0700

    added model data for Trip and Location

--- models/Location.js
+++ models/Location.js
@@ -5,7 +5,26 @@ const sequelize = require('../config/connection.js');
 class Location extends Model {}
 
 Location.init(
-  
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        notNull: true,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        notNull: true,
+        isAlphanumeric: true,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Location',
+  }
 );
 
 module.exports = Location;
--- models/Trip.js
+++ models/Trip.js
@@ -2,10 +2,49 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Trip extends Model {}
+const Traveler = require('./Traveler.js')
+const Location = require('./Location.js')
+
+class Trip extends Model { }
 
 Trip.init(
-  
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            notNull: true,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        trip_budget: {
+            type: DataTypes.DECIMAL,
+            isDecimal: true,
+        },
+        traveler_amount: {
+            type: DataTypes.INTEGER,
+            isInt: true,
+        },
+        traveler_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: Traveler,
+                key: 'id'
+            }
+        },
+        location_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: Location,
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Trip',
+      }
 );
 
 module.exports = Trip;
